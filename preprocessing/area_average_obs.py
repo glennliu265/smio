dd2be34323a35fb21803722c030df76934156e32
Glenn Liu <glenn.y.liu@gmail.com>
2025-05-14 18:33:29 -0400

    Calculate Area-Average for CESM2 and ERA5

---
+++ preprocessing/area_average_obs.py
@@ -0,0 +1,249 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+
+Calculate Average SST for several observational products
+
+Created on Thu May  8 09:15:56 2025
+
+@author: gliu
+"""
+
+import time
+import numpy as np
+import numpy.ma as ma
+import matplotlib.pyplot as plt
+import xarray as xr
+import sys
+import tqdm
+import glob 
+import scipy as sp
+import cartopy.crs as ccrs
+from scipy.io import loadmat
+
+#%%
+
+# local device (currently set to run on Astraeus, customize later)
+amvpath = "/Users/gliu/Downloads/02_Research/01_Projects/01_AMV/00_Commons/03_Scripts/" # amv module
+scmpath = "/Users/gliu/Downloads/02_Research/01_Projects/01_AMV/02_stochmod/03_Scripts/stochmod/model/"
+
+sys.path.append(amvpath)
+sys.path.append(scmpath)
+
+from amv import proc,viz
+import scm
+import amv.loaders as dl
+import cvd_utils as cvd
+
+#%% indicate cropping region and check for a folder
+
+# Indicate Path to Area-Average Files
+dpath_aavg      = "/Users/gliu/Downloads/02_Research/01_Projects/05_SMIO/01_Data/region_average/"
+regname         = "SPGNE"
+bbsel           = [-40,-15,52,62]
+
+regname         = "NNAT"
+bbsel           = [-80,0,20,60]
+
+
+# Set up Output Path
+locfn,locstring = proc.make_locstring_bbox(bbsel,lon360=True)
+bbfn            = "%s_%s" % (regname,locfn)
+outpath         = "%s%s/" % (dpath_aavg,bbfn)
+proc.makedir(outpath)
+print("Region is        : %s" % bbfn)
+print("Output Path is   : %s" % outpath)
+
+# String Format is <bbfn>/<expname>_<vname>_<ystart>_<yend>_<procstring>.nc
+
+# ============================================
+#%% Preprocess for HadISST (already detrended)
+# ============================================
+
+# I think this was preprocessed in a much earlier script...
+# Need to find source and include newer years.
+
+expname     = "HadISST"
+vname       = "SST"
+ystart      = 1920
+yend        = 2017
+procstring  = "detrend_deseason"
+vname_out       = 'sst'
+outname     = "%s%s_%s_%s_%s_%s.nc" % (outpath,expname,vname_out,ystart,yend,procstring)
+print(outname)
+
+
+dpathraw    = "/Users/gliu/Downloads/02_Research/01_Projects/01_AMV/00_Commons/01_Data/"
+rawnc       = "HadISST_Detrended_Deanomalized_1920_2018.nc"
+
+st          = time.time()
+dsfull      = xr.open_dataset(dpathraw + rawnc) # Lon180 x Lat x Time
+dsreg       = proc.sel_region_xr(dsfull,bbsel).load()
+proc.printtime(st,print_str='Loaded')
+aavg        = proc.area_avg_cosweight(dsreg)
+try:
+    aavg            = aavg.rename(vname_out)
+except:
+    aavg = aavg.rename({vname:vname_out})
+edict       = proc.make_encoding_dict(aavg)
+aavg.to_netcdf(outname,encoding=edict)
+print("Loaded in %.2fs" % (time.time()-st))
+
+# Make a debugging plot
+dsreg[vname].isel(time=0).T.plot()
+
+
+
+# ============================================
+#%% Do for ERA5, Qnet, SST
+# ============================================
+
+expname     = "ERA5"
+vname       = "sst"
+ystart      = 1979
+yend        = 2024
+procstring  = "raw_IceMask5"
+outname     = "%s%s_%s_%s_%s_%s.nc" % (outpath,expname,vname,ystart,yend,procstring)
+print(outname)
+
+#
+dpathraw    = "/Users/gliu/Downloads/02_Research/01_Projects/05_SMIO/01_Data/"
+rawnc       = "ERA5_%s_NAtl_1979to2024.nc" % vname
+st          = time.time()
+dsfull      = xr.open_dataset(dpathraw + rawnc) # Lon180 x Lat x Time
+dsreg       = proc.sel_region_xr(dsfull,bbsel).load()[vname]
+
+# Load Ice Mask and Apply
+icemask     = dl.load_mask(expname="ERA5").mask
+dsreg       = dsreg * icemask
+proc.printtime(st,print_str='Loaded')
+
+# Take Area Average and Save
+aavg        = proc.area_avg_cosweight(dsreg)
+aavg        = aavg.rename(vname)
+edict       = proc.make_encoding_dict(aavg)
+aavg.to_netcdf(outname,encoding=edict)
+print("Loaded in %.2fs" % (time.time()-st))
+
+
+# =============================================
+#%% Do for OISST SST
+# =============================================
+
+expname         = "OISST"
+vname           = "sst"
+ystart          = 1982
+yend            = 2020
+procstring      = "raw_IceMaskMax5"
+outname         = "%s%s_%s_%s_%s_%s.nc" % (outpath,expname,vname,ystart,yend,procstring)
+print(outname)
+
+# Note: Copied from hfcalc/reanalysis/proc folder on 2025.05.13
+dpathraw        = "/Users/gliu/Downloads/02_Research/01_Projects/05_SMIO/01_Data/"
+rawnc           = "OISST_sst_1982_2020_NATL.nc"
+st              = time.time()
+dsfull          = xr.open_dataset(dpathraw + rawnc) # Lon180 x Lat x Time
+dsreg           = proc.sel_region_xr(dsfull,bbsel).load()[vname]
+
+# Load Ice Mask and Apply
+dpath_ice       = "/Users/gliu/Downloads/02_Research/01_Projects/01_AMV/01_hfdamping/01_Data/reanalysis/proc/NATL_proc_obs/"
+nc_masks        = dpath_ice + "OISST_ice_masks_1981_2020.nc"
+ds_masks        = xr.open_dataset(nc_masks).load().mask_mon
+icemask         = xr.where(ds_masks==0,np.nan,1)
+icemask,dsfull  = proc.resize_ds([icemask,dsfull])
+icemask         = xr.where(np.isnan(dsfull.sst.isel(time=0)),np.nan,icemask)
+dsreg           = dsreg * icemask
+proc.printtime(st,print_str='Loaded')
+
+# Take Area Average and Save
+aavg            = proc.area_avg_cosweight(dsreg)
+aavg            = aavg.rename(vname)
+edict           = proc.make_encoding_dict(aavg)
+aavg.to_netcdf(outname,encoding=edict)
+print("Loaded in %.2fs" % (time.time()-st))
+
+# ===========================================
+#%% Preprocess for... EN4?
+# ===========================================
+# Note: Ice Mask is not Applied!
+
+expname         = "EN4"
+vname           = "temperature"
+ystart          = 1900
+yend            = 2021
+procstring      = "raw"
+outname         = "%s%s_%s_%s_%s_%s.nc" % (outpath,expname,vname_out,ystart,yend,procstring)
+print(outname)
+vname_out       = 'sst'
+
+dpathraw        = "/Users/gliu/Downloads/02_Research/01_Projects/01_AMV/00_Commons/01_Data/EN4/proc/"
+rawnc           = "EN4_concatenate_1900to2021_lon-80to00_lat00to65.nc"
+st              = time.time()
+dsfull          = xr.open_dataset(dpathraw + rawnc) # Lon180 x Lat x Time
+dsreg           = proc.sel_region_xr(dsfull,bbsel).load()[vname]
+proc.printtime(st,print_str='Loaded')
+
+# Take Area Average and Save
+aavg            = proc.area_avg_cosweight(dsreg)
+try:
+    aavg            = aavg.rename(vname_out)
+except:
+    aavg = aavg.rename({vname:vname_out})
+edict           = proc.make_encoding_dict(aavg)
+aavg.to_netcdf(outname,encoding=edict)
+print("Loaded in %.2fs" % (time.time()-st))
+
+# %% ERSST 5
+#
+
+expname     = "ERSST5"
+vname       = "sst"
+ystart          = 1854
+yend            = 2017
+procstring      = "raw"
+outname         = "%s%s_%s_%s_%s_%s.nc" % (outpath,expname,vname,ystart,yend,procstring)
+print(outname)
+vname_out       = 'sst'
+
+rawnc       = "ERSST5.nc"
+dpathraw    = "/Users/gliu/Downloads/02_Research/01_Projects/01_AMV/00_Commons/01_Data/"
+st              = time.time()
+dsfull          = xr.open_dataset(dpathraw + rawnc) # Lon180 x Lat x Time
+dsreg           = proc.sel_region_xr(dsfull,bbsel).load()[vname]
+proc.printtime(st,print_str='Loaded')
+
+# Select time
+dsreg = dsreg.sel(time=slice(None,'2017-12-31'))
+
+# Take Area Average and Save
+aavg            = proc.area_avg_cosweight(dsreg)
+try:
+    aavg            = aavg.rename(vname_out)
+except:
+    aavg = aavg.rename({vname:vname_out})
+edict           = proc.make_encoding_dict(aavg)
+aavg.to_netcdf(outname,encoding=edict)
+print("Loaded in %.2fs" % (time.time()-st))
+
+# =================
+# %% Glorys12V1 (Note this was for salt...)
+# =================
+
+# expname         = "glorys12v1"
+# vname           = "temperature"
+# ystart          = 1993
+# yend            = 2019
+# procstring      = "raw"
+# outname         = "%s%s_%s_%s_%s_%s.nc" % (outpath,expname,vname,ystart,yend,procstring)
+# print(outname)
+# vname_out       = 'sst'
+
+
+# dpathraw   = "/Users/gliu/Downloads/02_Research/01_Projects/01_AMV/00_Commons/01_Data/glorys12v1/"
+# rawnc      = "glorys12v1_so_NAtl_1993_2019_merge.nc"
+# st              = time.time()
+# dsfull          = xr.open_dataset(dpathraw + rawnc) # Lon180 x Lat x Time
+# dsreg           = proc.sel_region_xr(dsfull,bbsel).load()[vname]
+# proc.printtime(st,print_str='Loaded')
+
+
--- preprocessing/area_average_output.py
+++ preprocessing/area_average_output.py
@@ -27,8 +27,7 @@ import cartopy.crs as ccrs
 
 from scipy.io import loadmat
 
-#%%
-# stormtrack
+#%% stormtrack modules
 
 amvpath = "/home/glliu/00_Scripts/01_Projects/00_Commons/" # amv module
 scmpath = "/home/glliu/00_Scripts/01_Projects/01_AMV/02_stochmod/stochmod/model/" # scm module
@@ -40,30 +39,99 @@ from amv import proc,viz
 import scm
 import amv.loaders as dl
 
+#%% User Edits: Set up Region Average Directory
+
+# Indicate Path to Area-Average Files
+dpath_aavg      = "/stormtrack/data3/glliu/01_Data/02_AMV_Project/05_SMIO/data/region_average/"
+regname         = "SPGNE"
+bbsel           = [-40,-15,52,62]
+
+# regname         = "NNAT"
+# bbsel           = [-80,0,20,60]
+
+# Set up Output Path
+locfn,locstring = proc.make_locstring_bbox(bbsel,lon360=True)
+bbfn            = "%s_%s" % (regname,locfn)
+outpath         = "%s%s/" % (dpath_aavg,bbfn)
+proc.makedir(outpath)
+
+
 #%% User Edits
 
-smoutput = True # Set to True for stochastic model output
 
-# If smoutput is True...
+smoutput = False # Set to True for stochastic model output
+
+# outformat = "{outpath}{expname}_{vname}_{ystart:04d}_{yend:04d}_{procstring}.nc"
+# outname    = outformat.format(outpath=outpath,
+#                               expname=expname,
+#                               vname=vname,
+#                               ystart=ystart,
+#                               yend=yend,
+#                               procstring=procstring)#"CESM2_POM3_SHF_0200"
+
+outformat = "%s%s_%s_%04d_%04d_%s.nc"
+
+# If smoutput is <True>... ----------------------------------------------------
+# Use sm loader and output path to metrics folder
 expname  = "SST_ERA5_1979_2024"
 vname    = "SST"
+
+concat_dim = "ens"
+
 if smoutput:
     sm_output_path = "/stormtrack/data3/glliu/01_Data/02_AMV_Project/03_reemergence/sm_experiments/"
     outpath        = "%s%s/Metrics/" % (sm_output_path,expname) # Save into experiment directory
-
-# Otherwise, specify an ncsearch
-#ncpath = ""
-ncsearch = ""
-
-
-# Indicate cropping region
-regname = "SPGNE"
-bbsel   = [-40,-15,52,62]
-locfn,locstring = proc.make_locstring_bbox(bbsel,lon360=True)
-bbfn    = "%s_%s" % (regname,locfn)
-
-if smoutput:
+    # Output path to "Metrics" Folder of stochastic model output...
     outname = "%sArea_Avg_%s.nc" % (outpath,bbfn)
+
+
+# Otherwise specify an ncsearch
+
+# CESM2_POM3_PiControl_SHF_0100_0500_raw.nc ~~~~~~~~~~~~~~~~
+concat_dim = "time"
+vname      = "TS"
+expname    = "CESM2_POM3_PiControl"
+ystart     = 000
+yend       = 500
+procstring = "raw"
+searchpath = "/stormtrack/data4/glliu/01_Data/CESM2_PiControl/POM/"
+ncsearch   = searchpath + "*TS.nc"
+
+# CESM2_FCM_PiControl_SHF_ ~~~~~~~~~~~~~~~~
+# concat_dim = "time"
+# vname      = "SHF"
+# expname    = "CESM2_FCM_PiControl"
+# ystart     = 0000
+# yend       = 2000
+# procstring = "raw"
+# searchpath = "/stormtrack/data4/glliu/01_Data/CESM2_PiControl/FCM/ocn/SHF/"
+# ncsearch   = searchpath + "*SHF*.nc"
+
+# # CESM2_FCM_PiControl_SST_ ~~~~~~~~~~~~~~~~
+# concat_dim = "time"
+# vname      = "SST"
+# expname    = "CESM2_FCM_PiControl"
+# ystart     = 0000
+# yend       = 2000
+# procstring = "raw"
+# searchpath = "/stormtrack/data4/glliu/01_Data/CESM2_PiControl/FCM/ocn/SST/"
+# ncsearch   = searchpath + "*SST*.nc"
+
+# SOM TS
+concat_dim = "time"
+vname      = "LHFLX"#"FLNS"#"TS"
+expname    = "CESM2_SOM_PiControl"
+ystart     = 0
+yend       = 360
+procstring = "raw"
+searchpath = "/stormtrack/data4/glliu/01_Data/CESM2_PiControl/SOM/"
+ncsearch   = searchpath + "*%s*.nc" % vname
+
+outname    = outformat % (outpath,expname,vname,ystart,yend,procstring)
+
+print("Region is        : %s" % bbfn)
+print("Output Path is   : %s" % outpath)
+print("Output file is   : %s" % outname)
 
 #%% Declare some functions
 
@@ -96,8 +164,19 @@ for ff in tqdm.tqdm(range(nfiles)):
     
     ncname = nclist[ff]
     ds     = xr.open_dataset(ncname)
-    dsreg  = proc.sel_region_xr(ds,bbsel).load()
-    dsavg  = proc.area_avg_cosweight(dsreg)
+    
+    if 'lon' in ds.coords:
+        ds     = proc.lon360to180_xr(ds)
+        dsreg  = proc.sel_region_xr(ds,bbsel)[vname].load()
+        dsavg  = proc.area_avg_cosweight(dsreg)
+        
+    elif "TLONG" in ds.coords:
+        dsreg = proc.sel_region_xr_cv(ds,bbsel).load()
+        dsavg = proc.area_avg_cosweight_cv(dsreg,vname)
+        
+        # Sanity Check
+        #plt.scatter(dsreg.TLONG,dsreg.TLAT,c=dsreg[vname].isel(time=0)),plt.show()
+    
     aavgs.append(dsavg)
     
 
@@ -112,15 +191,17 @@ for ff in tqdm.tqdm(range(nfiles)):
 #     da_out  = reshape_ens2year(da_in)
     
 if len(nclist) > 1:
-    aavgs   = xr.concat(aavgs,dim='ens')
-    da_in   = aavgs[vname]
-    da_out  = reshape_ens2year(da_in)
+    if concat_dim == 'ens':
+        aavgs   = xr.concat(aavgs,dim='ens')
+        da_in   = aavgs[vname]
+        da_out  = reshape_ens2year(da_in)
+    elif concat_dim == "time":
+        da_out  =  xr.concat(aavgs,dim='time')
 else:
     
     da_out = aavgs[0]
     
 edict = proc.make_encoding_dict(da_out)
 da_out.to_netcdf(outname,encoding=edict)
-    
 print("File saved to %s" % outname)
 
