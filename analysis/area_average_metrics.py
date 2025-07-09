873b8c76c0a689885cfc516c20b64d73040f4e57
Glenn Liu <glenn.y.liu@gmail.com>
2025-07-09 18:11:40 -0400

    Added outline figures
    - Combined ACF (Winter/Summer)
    - Combined barplots and power spectra

--- analysis/area_average_metrics.py
+++ analysis/area_average_metrics.py
@@ -112,7 +112,7 @@ def plot_ice_ssh(fsz_ticks=20-2,label_ssh=False):
 #%% Further User Edits (Set Paths, Load other Data)
 
 # Set Paths
-figpath         = "/Users/gliu/Downloads/02_Research/01_Projects/05_SMIO/02_Figures/20250523/"
+figpath         = "/Users/gliu/Downloads/02_Research/01_Projects/05_SMIO/02_Figures/20250711/"
 sm_output_path  = "/Users/gliu/Downloads/02_Research/01_Projects/01_AMV/03_reemergence/01_Data/sm_experiments/"
 proc.makedir(figpath)
 
@@ -157,6 +157,63 @@ expnames_short  = ["SM","SM_REM","ERA5"]
 expcols         = ["goldenrod","turquoise","k"]
 expls           = ["dashed","dotted",'solid']
 
+# ---
+# (4) Try Different Significance lEvels
+comparename     = "SigLevelTest"
+expnames        = ["SST_Obs_Pilot_00_Tdcorr1_qnet_noPositive_SPGNE",
+                   "SST_Obs_Pilot_00_Tdcorr0_qnet_noPositive",
+                   "SST_Obs_Pilot_00_Tdcorr0_qnet",
+                   "SST_Obs_Pilot_00_Tdcorr0_qnet_p10_SPGNE",
+                   "SST_Obs_Pilot_00_Tdcorr0_qnet_p20_SPGNE",
+                   "SST_ERA5_1979_2024",
+                   ]
+expnames_long   = ["Remove Negative (no entrainment forcing)",
+                   "Remove Negative",
+                   "5% Significance",
+                   "10% Significance",
+                   "20% Significance",
+                   "ERA5"
+                   ]
+expnames_short  = ["NoNeg0","NoNeg1","5","10","20","ERA5"]
+expcols         = ["goldenrod","turquoise",'midnightblue','blue','cornflowerblue',"k"]
+expls           = ["dotted","dotted",'dashed','dashed','dashed','solid']      
+
+# (5) Add Subsurface Damping EN4
+comparename     = "LbddEN4"
+expnames        = [
+                   "SST_Obs_Pilot_00_qnet_AConly_SPGNE_addlbdd",
+                   #"SST_Obs_Pilot_00_qnet_AConly_SPG_addlbdd",
+                   "SST_Obs_Pilot_00_Tdcorr0_qnet_noPositive",
+                   "SST_Obs_Pilot_00_Tdcorr0_qnet_p10_SPGNE",
+                   "SST_ERA5_1979_2024",
+                   ]
+expnames_long   = ["Add Subsurface Damping",
+                   #"Add Subsurface Damping (with updated forcing)",
+                   "No Positive Feedback",
+                   "10% Significance",
+                   "ERA5"
+                   ]
+expnames_short  = ["Lbdd","10","ERA5"] # "LbddUpdate",
+expcols         = ["turquoise",'midnightblue','blue','cornflowerblue',"k"]
+expls           = ["dotted",'dashed','dashed','solid']                  
+
+
+# (6) Updated Versionw ith EN4 Damping and All-Feedback Forcing
+comparename     = "OutlineInProg"
+expnames        = ["SST_Obs_Pilot_00_qnet_AConly_SPGNE_noREM","SST_Obs_Pilot_00_qnet_AConly_SPG_addlbdd","SST_ERA5_1979_2024"]
+expnames_long   = ["Stochastic Model","Stochastic Model (with re-emergence)","ERA5"]
+expnames_short  = ["SM","SM_REM","ERA5"]
+expcols         = ["goldenrod","turquoise","k"]
+expls           = ["dashed","dotted",'solid']
+
+# (6) Updated Versionw ith EN4 Damping and All-Feedback Forcing
+comparename     = "OutlineORAS5"
+expnames        = ["SST_Obs_Pilot_00_qnet_AConly_SPGNE_noREM","SST_Obs_Pilot_00_qnet_AConly_SPGNE_ORAS5","SST_ERA5_1979_2024"]
+expnames_long   = ["Stochastic Model","Stochastic Model (with re-emergence)","ERA5"]
+expnames_short  = ["SM","SM (+REM)","ERA5"]
+expcols         = ["goldenrod","turquoise","k"]
+expls           = ["dashed","dotted",'solid']
+
 #%% Load information for each region
 
 dsall = []
@@ -164,8 +221,6 @@ nexps = len(expnames)
 for ex in tqdm.tqdm(range(nexps)):
     expname = expnames[ex]
     ncname  = "%s%s/Metrics/Area_Avg_%s.nc" % (sm_output_path,expname,bbfn)
-    
-
     
     ds      = xr.open_dataset(ncname).load()
     dsall.append(ds)
@@ -204,7 +259,8 @@ if (detrend_obs_regression):
 
 ssts         = [ds.data for ds in ssts_ds]
 lags         = np.arange(61)
-nsmooths     = [250,250,4]
+nsmooths     = [250,] * (nexps-1) + [4,]
+ #nsmooths     = [250,250,4]
 metrics_out  = scm.compute_sm_metrics(ssts,nsmooth=nsmooths,lags=lags)
 
 #%% Compute some additional metrics
@@ -214,7 +270,7 @@ ssts_lp  = [proc.lp_butter(ts,120,6) for ts in ssts_ds]
 stds_lp  = np.array([np.std(ds) for ds in ssts_lp])
 vratio   = (stds_lp  / stds) * 100
 
-#%%
+#%% Plot ACF
 kmonth = 1
 xtks   = lags[::6]
 conf   = 0.95
@@ -225,9 +281,17 @@ for kmonth in range(12):
     
     for ex in range(nexps):
         
+        if ex == nexps-1:
+            col_in = dfcol
+        else:
+            col_in = expcols[ex]
+        
         plotvar = metrics_out['acfs'][kmonth][ex]
+        
+        
+        
         ax.plot(lags,plotvar,
-                label=expnames_long[ex],color=expcols[ex],ls=expls[ex],lw=2.5)
+                label=expnames_long[ex],color=col_in,ls=expls[ex],lw=2.5)
         
         
         # Calcualate Confidence Interval
@@ -235,10 +299,13 @@ for kmonth in range(12):
         
         cflag = proc.calc_conflag(plotvar,conf,2,len(ssts[ex])/12)
         if ex == 2:
-            alpha = 0.05
+            if darkmode:
+                alpha = 0.15
+            else:
+                alpha = 0.05
         else:
             alpha = 0.15
-        ax.fill_between(lags,cflag[:,0],cflag[:,1],alpha=alpha,color=expcols[ex],zorder=3)
+        ax.fill_between(lags,cflag[:,0],cflag[:,1],alpha=alpha,color=col_in,zorder=3)
         
     ax.legend()
     
@@ -249,28 +316,41 @@ for kmonth in range(12):
     
     
     figname = "%sACF_%s_mon%02i.png" % (figpath,comparename,kmonth+1)
-    plt.savefig(figname,dpi=150)
+    if darkmode:
+        figname = proc.darkname(figname)
+        #figname = proc.addstrtoext(figname,"_darkmode")
+    plt.savefig(figname,dpi=150,transparent=transparent)
+
 
 
 #%% Plot Spectra
+decadal_focus = False
+
 dtmon_fix       = 60*60*24*30
-xper            = np.array([40,10,5,1,0.5])
+
+if decadal_focus:
+    xper            = np.array([10,5,1,0.5])
+else:
+    xper            = np.array([40,10,5,1,0.5])
 xper_ticks      = 1 / (xper*12)
 
-    
 fig,ax      = plt.subplots(1,1,figsize=(8,4.5),constrained_layout=True)
 
 for ii in range(nexps):
+    if ii == nexps-1:
+        col_in = dfcol
+    else:
+        col_in = expcols[ii]
     plotspec        = metrics_out['specs'][ii] / dtmon_fix
     plotfreq        = metrics_out['freqs'][ii] * dtmon_fix
     CCs = metrics_out['CCs'][ii] / dtmon_fix
 
-    ax.loglog(plotfreq,plotspec,lw=2.5,label=expnames_long[ii],c=expcols[ii])
+    ax.loglog(plotfreq,plotspec,lw=2.5,label=expnames_long[ii],c=col_in)
     
     #ax.loglog(plotfreq,CCs[:,0],ls='dotted',lw=0.5,c=expcols[ii])
     #ax.loglog(plotfreq,CCs[:,1],ls='dashed',lw=0.9,c=expcols[ii])
 
-ax.set_xlim([1/1000,0.5])
+ax.set_xlim([xper_ticks[0],0.5])
 ax.axvline([1/(6)],label="",ls='dotted',c='gray')
 ax.axvline([1/(12)],label="",ls='dotted',c='gray')
 ax.axvline([1/(5*12)],label="",ls='dotted',c='gray')
@@ -281,7 +361,7 @@ ax.set_xlabel("Frequency (1/Month)",fontsize=14)
 ax.set_ylabel("Power [$\degree C ^2 cycle \, per \, mon$]")
 
 ax2 = ax.twiny()
-ax2.set_xlim([1/1000,0.5])
+ax2.set_xlim([xper_ticks[0],0.5])
 ax2.set_xscale('log')
 ax2.set_xticks(xper_ticks,labels=xper)
 ax2.set_xlabel("Period (Years)",fontsize=14)
@@ -292,12 +372,14 @@ alpha           = 0.05
 cloc_era        = [plotfreq[0],1e-1]
 dof_era         = metrics_out['dofs'][-1]
 cbnds_era       = proc.calc_confspec(alpha,dof_era)
-proc.plot_conflog(cloc_era,cbnds_era,ax=ax,color='k',cflabel=r"95% Confidence") #+r" (dof= %.2f)" % dof_era)
+proc.plot_conflog(cloc_era,cbnds_era,ax=ax,color=dfcol,cflabel=r"95% Confidence") #+r" (dof= %.2f)" % dof_era)
 
 ax.legend()
 
 figname = "%sSpectra_LogLog_%s.png" % (figpath,comparename)
-plt.savefig(figname,dpi=150,bbox_inches='tight')
+if darkmode:
+    figname = proc.addstrtoext(figname,"_darkmode")
+plt.savefig(figname,dpi=150,bbox_inches='tight',transparent=transparent)
     
 #%% Plot ACF Map
 
@@ -345,35 +427,257 @@ for ex in range(nexps):
 
 #%% Make Barplot
 
+remove_topright = True
 expcols_bar     = np.array(expcols).copy()
 expcols_bar[-1] = 'gray'
+label_vratio    = False
+
+fsz_axis        = 18
+fsz_tick        = 16
 
 instd           = stds
 instd_lp        = stds_lp
 
-xlabs           = ["%s\n%.2f" % (expnames_long[ii],vratio[ii])+"%" for ii in range(len(vratio))]
+if label_vratio:
+    xlabs           = ["%s\n%.2f" % (expnames_long[ii],vratio[ii])+"%" for ii in range(len(vratio))]
+else:
+    xlabs  = expnames_short
 fig,ax          = plt.subplots(1,1,constrained_layout=True,figsize=(6,6))
 
 braw            = ax.bar(np.arange(nexps),instd,color=expcols_bar)
-blp             = ax.bar(np.arange(nexps),instd_lp,color='k')
+blp             = ax.bar(np.arange(nexps),instd_lp,color=dfcol)
 
-ax.bar_label(braw,fmt="%.02f",c='gray')
-ax.bar_label(blp,fmt="%.02f",c='k')
+if darkmode:
+    upperlbl_col = 'lightgray'
+else:
+    upperlbl_col = 'gray'
+
+ax.bar_label(braw,fmt="%.02f",c=upperlbl_col,fontsize=fsz_axis)
+ax.bar_label(blp,fmt="%.02f",c=dfcol,fontsize=fsz_axis)
 
 #ax.bar_label(vratio,fmt="%.2f",c=w,label_type='bottom')
 
-ax.set_xticks(np.arange(nexps),labels=xlabs)
-ax.set_ylabel("$\sigma$(SST) [$\degree$C]")
+ax.set_xticks(np.arange(nexps),labels=xlabs,fontsize=fsz_tick,rotation=45)
+ax.set_ylabel("$\sigma$(SST) [$\degree$C]",fontsize=fsz_axis)
 ax.set_ylim([0,1.0])
+ax.tick_params(labelsize=fsz_tick)
 
+if remove_topright:
+    ax.spines[['right', 'top']].set_visible(False)
+    
 savename = "%sBarplots_%s_%s.png" % (figpath,comparename,expnames[ex])
-plt.savefig(savename,dpi=150,bbox_inches='tight')
+if darkmode:
+    figname = proc.addstrtoext(figname,"_darkmode")
+plt.savefig(savename,dpi=150,bbox_inches='tight',transparent=transparent)
 
-#%% Do F-test
+#%% Plot the Monthly Variance
+
+fsz_legend=12
+
+fig,ax = viz.init_monplot(1,1,figsize=(8,4.5))
+
+for ex in range(nexps):
+    
+    col_in = expcols[ex]
+    if darkmode and col_in =="k":
+        col_in = dfcol
+        
+    plotvar = metrics_out['monvars'][ex]
+    
+    ax.plot(mons3,plotvar,label=expnames_long[ex],c=col_in,lw=2.5,ls=expls[ex])
 
 
-dofs = [proc.calc_dof(ts) for ts in ssts]
+ax.set_ylim([-.2,.5])
+ax.set_ylabel("SST Variance [$\degree C^2$]",fontsize=fsz_axis)
+ax.tick_params(labelsize=fsz_tick)
+ax.legend(fontsize=fsz_legend,ncol=2)
+
+savename = "%sMonvar_%s.png" % (figpath,comparename)
+if darkmode:
+    figname = proc.addstrtoext(figname,"_darkmode")
+plt.savefig(savename,dpi=150,bbox_inches='tight',transparent=transparent)
 
 
-#proc.calc_dof
+# ====================================================
+#%% Plot ACF for Winter and Summer (For Paper Outline)
+# ====================================================
 
+fsz_title = 26
+fsz_ticks = 14
+plotkmons = [1,6]
+fig,axs = plt.subplots(2,1,constrained_layout=True,figsize=(8,6.5))
+
+for ii in range(2):
+    ax = axs[ii]
+    kmonth = plotkmons[ii]
+    
+    ax,_   = viz.init_acplot(kmonth,xtks,lags,ax=ax,title="")
+    for ex in range(nexps):
+        
+        if ex == nexps-1:
+            col_in = dfcol
+        else:
+            col_in = expcols[ex]
+        
+        plotvar = metrics_out['acfs'][kmonth][ex]
+        
+        ax.plot(lags,plotvar,
+                label=expnames_long[ex],color=col_in,ls=expls[ex],lw=2.5)
+        
+        cflag = proc.calc_conflag(plotvar,conf,2,len(ssts[ex])/12)
+        if ex == 2:
+            if darkmode:
+                alpha = 0.15
+            else:
+                alpha = 0.05
+        else:
+            alpha = 0.15
+        ax.fill_between(lags,cflag[:,0],cflag[:,1],alpha=alpha,color=col_in,zorder=3)
+        
+    if ii == 0:
+        ax.set_xlabel("")
+    else:
+        ax.legend(framealpha=0,fontsize=fsz_ticks,ncol=2)
+        
+        
+    ax.set_ylim([-.25,1])
+    
+    ax.set_title("")
+    ax.set_ylabel("Correlation with \n %s. Anomalies" % (mons3[kmonth]),fontsize=fsz_tick)
+    
+    viz.label_sp(ii,alpha=0.15,ax=ax,fontsize=fsz_title,y=1.28,x=-.15,
+                 fontcolor=dfcol)
+    
+    
+    ax.tick_params(labelsize=fsz_ticks)
+    
+    
+figname = "%sACF_%s_PaperOutline.png" % (figpath,comparename)
+if darkmode:
+    figname = proc.darkname(figname)
+plt.savefig(figname,dpi=150,transparent=transparent)
+
+# ---------------------------------
+#%% Combine the Spectra and Barplot
+# ---------------------------------
+
+
+fig       = plt.figure(figsize=(14,4))
+gs        = gridspec.GridSpec(4,12)
+
+# --------------------------------- # Barplot
+ax11            = fig.add_subplot(gs[:,:3],)
+ax              = ax11
+
+remove_topright = True
+expcols_bar     = np.array(expcols).copy()
+expcols_bar[-1] = 'gray'
+label_vratio    = False
+
+fsz_axis         = 18
+fsz_ticks        = 16
+fsz_legend       = 14
+
+instd           = stds
+instd_lp        = stds_lp
+
+if label_vratio:
+    xlabs           = ["%s\n%.2f" % (expnames_long[ii],vratio[ii])+"%" for ii in range(len(vratio))]
+else:
+    xlabs  = expnames_short
+
+braw            = ax.bar(np.arange(nexps),instd,color=expcols_bar)
+blp             = ax.bar(np.arange(nexps),instd_lp,color=dfcol)
+
+if darkmode:
+    upperlbl_col = 'lightgray'
+else:
+    upperlbl_col = 'gray'
+
+ax.bar_label(braw,fmt="%.02f",c=upperlbl_col,fontsize=fsz_axis)
+ax.bar_label(blp,fmt="%.02f",c=dfcol,fontsize=fsz_axis)
+
+# --- Make Fake Legend'
+colorsf = {'Raw':'gray','10-year Low-pass':'k',}         
+labels = list(colorsf.keys())
+handles = [plt.Rectangle((0,0),1,1, color=colorsf[label]) for label in labels]
+ax.legend(handles, labels,fontsize=fsz_legend,framealpha=0)
+#ax.legend()
+#ax.bar_label(vratio,fmt="%.2f",c=w,label_type='bottom')
+
+ax.set_xticks(np.arange(nexps),labels=xlabs,fontsize=fsz_tick,rotation=45)
+ax.set_ylabel("$\sigma$(SST) [$\degree$C]",fontsize=fsz_axis)
+ax.set_ylim([0,1.0])
+ax.tick_params(labelsize=fsz_tick)
+
+if remove_topright:
+    ax.spines[['right', 'top']].set_visible(False)
+
+viz.label_sp(0,alpha=0.15,ax=ax,fontsize=fsz_title,y=1.10,x=-.45,
+             fontcolor=dfcol)
+
+
+
+# --------------------------------- # Power Spectra
+ax22       = fig.add_subplot(gs[:,4:])
+
+ax         = ax22
+
+decadal_focus = False
+
+dtmon_fix       = 60*60*24*30
+
+if decadal_focus:
+    xper            = np.array([10,5,1,0.5])
+else:
+    xper            = np.array([40,10,5,1,0.5])
+xper_ticks      = 1 / (xper*12)
+
+for ii in range(nexps):
+    if ii == nexps-1:
+        col_in = dfcol
+    else:
+        col_in = expcols[ii]
+    plotspec        = metrics_out['specs'][ii] / dtmon_fix
+    plotfreq        = metrics_out['freqs'][ii] * dtmon_fix
+    CCs = metrics_out['CCs'][ii] / dtmon_fix
+
+    ax.loglog(plotfreq,plotspec,lw=2.5,label=expnames_long[ii],c=col_in)
+    
+    #ax.loglog(plotfreq,CCs[:,0],ls='dotted',lw=0.5,c=expcols[ii])
+    #ax.loglog(plotfreq,CCs[:,1],ls='dashed',lw=0.9,c=expcols[ii])
+
+ax.set_xlim([xper_ticks[0],0.5])
+ax.axvline([1/(6)],label="",ls='dotted',c='gray')
+ax.axvline([1/(12)],label="",ls='dotted',c='gray')
+ax.axvline([1/(5*12)],label="",ls='dotted',c='gray')
+ax.axvline([1/(10*12)],label="",ls='dotted',c='gray')
+ax.axvline([1/(40*12)],label="",ls='dotted',c='gray')
+
+ax.set_xlabel("Frequency (1/month)",fontsize=fsz_axis)
+ax.set_ylabel("Power [$\degree C ^2 / cycle \, per \, mon$]",fontsize=fsz_axis)
+
+ax2 = ax.twiny()
+ax2.set_xlim([xper_ticks[0],0.5])
+ax2.set_xscale('log')
+ax2.set_xticks(xper_ticks,labels=xper)
+ax2.set_xlabel("Period (Years)",fontsize=fsz_axis)
+
+# Plot Confidence Interval (ERA5)
+alpha           = 0.05
+cloc_era        = [6e-2,5e-2]
+dof_era         = metrics_out['dofs'][-1]
+cbnds_era       = proc.calc_confspec(alpha,dof_era)
+proc.plot_conflog(cloc_era,cbnds_era,ax=ax,color=dfcol,cflabel=r"95% Confidence") #+r" (dof= %.2f)" % dof_era)
+ax.legend(fontsize=fsz_legend,framealpha=0.5,edgecolor='none')
+
+for ax in [ax,ax2]:
+    ax.tick_params(labelsize=fsz_ticks)
+    
+viz.label_sp(1,alpha=0.15,ax=ax,fontsize=fsz_title,y=1.10,x=-.1,
+             fontcolor=dfcol)
+
+figname = "%sVariance_%s_PaperOutline.png" % (figpath,comparename)
+if darkmode:
+    figname = proc.darkname(figname)
+plt.savefig(figname,dpi=150,transparent=transparent,bbox_inches='tight')
