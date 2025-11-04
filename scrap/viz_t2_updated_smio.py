337688b4d954240c296de2c6fc2ff952e67ea000
Glenn Liu <glenn.y.liu@gmail.com>
2025-11-04 11:09:48 -1000

    Draft 2: Added T2 calculations from Martha

--- scrap/viz_t2_updated_smio.py
+++ scrap/viz_t2_updated_smio.py
@@ -10,8 +10,6 @@ Created on Fri Oct 17 15:14:10 2025
 
 """
 
-
-
 import time
 import numpy as np
 import numpy.ma as ma
@@ -58,27 +56,44 @@ eranc       = acfpath + "ERA5_NAtl_1979to2025_lag00to40_JFM_ensALL.nc"
 
 sm_monnc    = acfpath + "SM_SST_ORAS5_avg_GMSST_EOFmon_usevar_NATL_lag00to40_JFM_ensALL.nc"
 
+exppath     = "/Users/gliu/Downloads/02_Research/01_Projects/01_AMV/03_reemergence/01_Data/sm_experiments/SST_ORAS5_avg_GMSST_EOFmon_usevar_NATL/Metrics/"
+blowupnc    = exppath + "Blowup_Points.nc"
+
 dsera       = xr.open_dataset(eranc).load()
 dssm        = xr.open_dataset(smnc).load()
 dssm_mon    = xr.open_dataset(sm_monnc).load()
+dsblowup    = xr.open_dataset(blowupnc).load()
 
 
 # Also load the monthly version
 
 sm_monthly_nc = acfpath + "SM_SST_ORAS5_avg_GMSST_EOFmon_usevar_NATL_lag00to60_ALL_ensALL.nc"
-dsmonthly  = xr.open_dataset(sm_monthly_nc).load()
+dsmonthly_nc  = xr.open_dataset(sm_monthly_nc).load()
 
 
 figpath     = "/Users/gliu/Downloads/02_Research/01_Projects/05_SMIO/02_Figures/20251106/"
 proc.makedir(figpath)
 
-
-
-
 #%% Also Load the MLD
 
 ncmld       = "/Users/gliu/Downloads/02_Research/01_Projects/01_AMV/03_reemergence/01_Data/proc/model_input/mld/MIMOC_regridERA5_h_pilot.nc"
 dsmld       = xr.open_dataset(ncmld).load()
+
+#%%
+fpath="/Users/gliu/Downloads/02_Research/01_Projects/05_SMIO/01_Data/"
+fn = "T2_SSTwint_regOutGM_arYW_AICr_remPolyOrder0_1950-2024.nc"
+ds2 = xr.open_dataset(fpath+fn).load()
+
+lonm         = np.linspace(0,360,ds2.XC.shape[1])#ds2.XC#inds[0].lon
+latm         = np.linspace(-90,90,ds2.YC.shape[0])#ds2.YC#inds[0].lat
+coords     = dict(lat=latm,lon=lonm,)
+ds2_martha  = xr.DataArray(np.flip(ds2.T2.data,0),coords=coords,dims=coords,name="T2")
+ds2_martha  = proc.lon360to180_xr(ds2_martha)
+ds2_martha = proc.sel_region_xr(ds2_martha,[-80,0,0,90])
+
+#ds2_martha   = ds2_martha.reindex(lat=list(reversed(ds2_martha.lat)))
+
+
 
 #%% Calculate T2
 
@@ -105,8 +120,6 @@ t2s  = [t2_era,t2_sm.mean(0)]
 import cmcrameri.cm as cm
 
 proj = ccrs.PlateCarree()
-
-
 
 plt.style.use('default')
 
@@ -215,18 +228,18 @@ for a,ax in enumerate(axs):
     
     # Plot the t2
     if a == 0:
-        plotvar = t2s[a].T #* dsmaskera.data
-        lon     = inds[a].lon
-        lat     = inds[a].lat
-        cmap    = cm.acton_r
-        cints   = np.arange(0,5.5,0.5)#np.arange(0,36,3)
-        cints_lab = cints[::2]
-        clab    = "Decorrelation Timescale $T^2$ [Years]"
+        plotvar     = t2s[a].T #* dsmaskera.data
+        lon         = inds[a].lon
+        lat         = inds[a].lat
+        cmap        = cm.devon_r#'cmo.dense'#cm.acton_r
+        cints       = np.arange(0,5.5,0.5)#np.arange(0,36,3)
+        cints_lab   = cints[::2]
+        clab        = "Decorrelation Timescale $T^2$ [Years]"
     elif a == 1:
         plotvar = dsmld.h.max('mon')# * dsmasksm
         lon     = plotvar.lon
         lat     = plotvar.lat
-        cmap    = "cmo.ice_r"
+        cmap    = 'cmo.ice_r'
         cints   = np.arange(0,525,25)
         cints_lab = cints[::4]
         clab    = "Maximum Seasonal Mixed-Layer Depth [meters]"
@@ -249,13 +262,13 @@ for a,ax in enumerate(axs):
     # Plot the Median Sea Ice Concentration
     plotvar = dsice
     icel    = ax.contour(plotvar.longitude,plotvar.latitude,plotvar.siconc_winter_median,levels=[0.15,],
-                      colors='yellow',linewidths=4,transform=proj,linestyles='dotted')
+                      colors='cyan',linewidths=4,transform=proj,linestyles='dotted')
     
     
-    bb = viz.plot_box(bbox_spgne,ax=ax,color='magenta',linewidth=2.5,proj=proj)
+    bb = viz.plot_box(bbox_spgne,ax=ax,color='purple',linewidth=4.5,proj=proj)
     
 figname = figpath + "ERAT2_MLD_DraftPlot.png" 
-plt.savefig(figname,dpi=150,bbox_inches='tight')
+plt.savefig(figname,dpi=150,bbox_inches='tight',transparent=True)
 
 
 #%% 2025.10.24, Check Monthly vs Annual Average values
@@ -310,12 +323,210 @@ ax.grid(True,ls='dotted',color='gray')
 
 ax.set_title("ACF Comparison @ %s" % locstr)
 
-    
-    
 figname = figpath + "ACF_Comparison_%s.png" % locfn
 plt.savefig(figname,dpi=150,bbox_inches='tight')
 
+#%% Draft 2 Version: plot, side by side, the Max Wintertime MLD and the Persistence of ERA5 SST Anomalies
+
+pmesh           = False
+fsz_axis        = 22
+fsz_tick        = 18
+fig,axs,_       = viz.init_orthomap(1,2,bbsel,figsize=(20,12),centlat=centlat)
+
+for a,ax in enumerate(axs):
+    ii              = a
+    ax              = viz.add_coast_grid(ax,bbox=bbsel,line_color='dimgray',
+                                        fill_color="lightgray",fontsize=18)
+    
+    # Plot the t2
+    if a == 0:
+        plotvar = t2s[a].T #* dsmaskera.data
+        lon     = inds[a].lon
+        lat     = inds[a].lat
+        cmap    = cm.acton_r
+        cints   = np.arange(0,5.5,0.5)#np.arange(0,36,3)
+        cints_lab = cints[::2]
+        clab    = "Decorrelation Timescale $T^2$ [Years]"
+    elif a == 1:
+        plotvar = dsmld.h.max('mon')# * dsmasksm
+        lon     = plotvar.lon
+        lat     = plotvar.lat
+        cmap    = "cmo.ice_r"
+        cints   = np.arange(0,525,25)
+        cints_lab = cints[::4]
+        clab    = "Maximum Seasonal Mixed-Layer Depth [meters]"
+    
+    cf      = ax.contourf(lon,lat,plotvar,
+                          levels=cints,
+                          transform=proj,cmap=cmap,zorder=-1)
+    
+    cl      = ax.contour(lon,lat,plotvar,
+                          levels=cints_lab,linewidths=.55,colors='w',
+                          transform=proj,zorder=-1)
+    ax.clabel(cl,fontsize=fsz_tick)
+    
+    # clbl    = ax.clabel(cf,levels=cints[:-2:2])
+    # viz.add_fontborder(clbl,w=1)
+    
+    cb = viz.hcbar(cf,ax=ax,fontsize=22,pad=0.0001,fraction=0.045)
+    cb.set_label(clab,fontsize=fsz_axis)  
+    
+    # Plot the Median Sea Ice Concentration
+    plotvar = dsice
+    icel    = ax.contour(plotvar.longitude,plotvar.latitude,plotvar.siconc_winter_median,levels=[0.15,],
+                      colors='yellow',linewidths=4,transform=proj,linestyles='dotted')
+    
+    
+    bb = viz.plot_box(bbox_spgne,ax=ax,color='magenta',linewidth=2.5,proj=proj)
+    
+figname = figpath + "ERAT2_MLD_DraftPlot.png" 
+plt.savefig(figname,dpi=150,bbox_inches='tight')
+
+#%% Demarcate points blown up
+
+nanids           = dsblowup.min('run').nanid
+blowupmask       = xr.where(nanids > 0,1,0)
+blowupmask_apply = xr.where(nanids > 0,np.nan,1)
+
+#%% Fix this fucking image
+
+fig,ax,_       = viz.init_orthomap(1,1,bbsel,figsize=(24,12),centlat=centlat,centlon=centlon)
+ax             = viz.add_coast_grid(ax,bbox=bbsel,line_color='k',
+                                    fill_color="lightgray",fontsize=fsz_tick,
+                                    fix_lon=fix_lon,fix_lat=fix_lat)
+
+
+plotvar     = ds2_martha#.T2 #ds2.T2.data #t2s[0].T #* dsmaskera.data
+#plotvar     = proc.sel_region_xr(plotvar,[-80,0,0,65])
+lon         = ds2_martha.lon#np.linspace(0,360,ds2.XC.shape[1])#ds2.XC#inds[0].lon
+lat         = ds2_martha.lat#np.linspace(90,-90,ds2.YC.shape[0])#ds2.YC#inds[0].lat
+ 
+pcm         = ax.contourf(lon,lat,plotvar,transform=proj,cmap=cmap)
 
 
 
+#%%
+pmesh           = False
+fsz_axis        = 22
+fsz_tick        = 18
+fsz_title       = 28
+
+use_marthas_t2 = True
+
+#Contour Settings
+cints_t2_lab    = np.arange(0,6,1)
+cints_t2        = np.arange(0,4.2,0.2)
+
+# Bounding Box
+bbsel           = [-65, -5, 45, 65] # [-40, -12, 50, 62]
+centlat         = 55
+centlon         = -35
+fix_lon         = np.arange(-65,5,5)
+fix_lat         = np.arange(45,70,5)
+
+mld_cbticks     = np.arange(0,600,100)
+t2_cbticks      = np.arange(0,5,1)
+
+fig,axs,_       = viz.init_orthomap(1,3,bbsel,figsize=(24,12),centlat=centlat,centlon=centlon)
+
+for a,ax in enumerate(axs):
+    ii              = a
+    ax              = viz.add_coast_grid(ax,bbox=bbsel,line_color='k',
+                                        fill_color="lightgray",fontsize=fsz_tick,
+                                        fix_lon=fix_lon,fix_lat=fix_lat)
+    
+    # if a != 2:
+    #     continue
+    # Plot the t2
+    if a == 1:
+        if use_marthas_t2:
+            plotvar     = ds2_martha#.T2 #ds2.T2.data #t2s[0].T #* dsmaskera.data
+           #plotvar     = proc.sel_region_xr(plotvar,[-80,0,0,65])
+            lon         = ds2_martha.lon#np.linspace(0,360,ds2.XC.shape[1])#ds2.XC#inds[0].lon
+            lat         = ds2_martha.lat#np.linspace(90,-90,ds2.YC.shape[0])#ds2.YC#inds[0].lat
+        else:
+            plotvar     = t2s[0].T #* dsmaskera.data
+            lon         = inds[0].lon
+            lat         = inds[0].lat
+        
+        cmap        = cm.devon_r#'cmo.dense'#cm.acton_r
+        cints       = cints_t2 #np.arange(0,5.5,0.5)
+        cints_lab   = cints_t2_lab#cints[::2]
+        clab        = "Decorrelation Timescale $T^2$ [Years]"
+        cbticks     = t2_cbticks
+        
+    elif a == 0:
+        plotvar     = dsmld.h.max('mon')# * dsmasksm
+        lon         = plotvar.lon
+        lat         = plotvar.lat
+        cmap        = 'cmo.ice_r'
+        cints       = np.arange(0,525,25)
+        cints_lab   = cints[::4]
+        clab        = "Maximum Climatological Mixed-Layer Depth [meters]"
+        
+        cbticks     = mld_cbticks
+        
+        
+    elif a == 2:
+        
+        plotvar     = t2s[1].T * blowupmask_apply.data #* dsmaskera.data
+        lon         = inds[1].lon
+        lat         = inds[1].lat
+        cmap        = cm.devon_r#'cmo.dense'#cm.acton_r
+        cints       = cints_t2#np.arange(0,4.2,0.2)#np.arange(0,5.5,0.5)#np.arange(0,36,3)
+        cints_lab   = cints_t2_lab #cints[::2]
+        clab        = "Decorrelation Timescale $T^2$ [Years]"
+        
+        cbticks     = t2_cbticks
+    
+
+        
+
+    
+    cf      = ax.contourf(lon,lat,plotvar,
+                          levels=cints,
+                          transform=proj,cmap=cmap,zorder=-1,extend='both')
+    
+    cl      = ax.contour(lon,lat,plotvar,
+                          levels=cints_lab,linewidths=.55,colors='w',
+                          transform=proj,zorder=-1)
+    ax.clabel(cl,fontsize=fsz_tick)
+    
+    # clbl    = ax.clabel(cf,levels=cints[:-2:2])
+    # viz.add_fontborder(clbl,w=1)
+    
+    if a == 0:
+        cb = viz.hcbar(cf,ax=ax,fontsize=22,pad=0.0001,fraction=0.040)
+        cb.set_label(clab,fontsize=fsz_axis)
+        cb.set_ticks(cbticks)
+    elif a == 2:
+        cb = viz.hcbar(cf,ax=axs[1:].flatten(),fontsize=22,pad=0.0001,fraction=0.040)
+        cb.set_label(clab,fontsize=fsz_axis)
+        cb.set_ticks(cbticks)
+    
+    # Plot the Median Sea Ice Concentration
+    plotvar = dsice
+    icel    = ax.contour(plotvar.longitude,plotvar.latitude,plotvar.siconc_winter_median,levels=[0.15,],
+                      colors='cyan',linewidths=4,transform=proj,linestyles='dotted')
+    
+    # # Plot Blow Up Points (for Stochastic Model Simulation)
+    # if a == 2:
+    #     plotvar = blowupmask
+    #     viz.plot_mask(plotvar.lon,plotvar.lat,plotvar.T,
+    #                   reverse=True,
+    #                   geoaxes=True,proj=proj,ax=ax,marker="x",markersize=10,color='hotpink')
+    
+    bb = viz.plot_box(bbox_spgne,ax=ax,color='purple',linewidth=4.5,proj=proj)
+    viz.label_sp(a,ax=ax,fig=fig,fontsize=fsz_title)
+    
+
+figname = figpath + "ERA_SM_T2_MLD_Draft02Plot.png" 
+plt.savefig(figname,dpi=150,bbox_inches='tight',transparent=True)
+
+
+plotvar     = ds2.T2.data #t2s[0].T #* dsmaskera.data
+lon         = np.linspace(0,360,ds2.XC.shape[1])#ds2.XC#inds[0].lon
+lat         = np.linspace(-90,90,ds2.YC.shape[0])#ds2.YC#inds[0].lat
+
+plt.pcolormesh(lon,lat,plotvar)
 
